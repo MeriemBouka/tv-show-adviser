cbef1129ca62b52fd09f8dbdeb5511a136b80358
Meriem Timdouine <meriem.boukaddouss@gmail.com>
2023-06-22 12:40:35 +0200

    image à afficher en cas de null

--- src/components/TVShowListItem/TVShowListItem.jsx
+++ src/components/TVShowListItem/TVShowListItem.jsx
@@ -1,12 +1,13 @@
 import s from "./style.module.css";
 import { SMALL_IMG_COVER_BASE_URL } from "../../config";
+import clapperboard from '../../assets/images/clapperboardMin.png'
 export function TVShowListItem({ tvShow, onClick }) {
   return (
     <div onClick={() => onClick(tvShow)} className={s.container}>
       <img
         className={s.img}
         alt={tvShow.original_title}
-        src={SMALL_IMG_COVER_BASE_URL + tvShow.backdrop_path}
+        src={tvShow.backdrop_path ? SMALL_IMG_COVER_BASE_URL + tvShow.backdrop_path : clapperboard}
       />
       <div className={s.title}>{tvShow.original_title}</div>
     </div>
